65cc915d7a4187a10ed37f0a16602cc752c4c05e
James White <124373979+white-james@users.noreply.github.com>
2024-09-09 10:42:54 +0100

    Update install-software.ps1

--- install-software/install-software.ps1
+++ install-software/install-software.ps1
@@ -1,26 +1,7 @@
 # install-software.ps1
 
-# Path where the Azure CustomScriptExtension downloads the files
-$downloadDirectory = "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.10.17\Downloads\1"
+# Install Chocolately
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# Paths to the downloaded child scripts
-$chocolateyScriptPath = "$downloadDirectory\Install-Chocolatey.ps1"
-$iisScriptPath = "$downloadDirectory\automate-iis.ps1"
-
-# Check if the Chocolatey script exists and execute it
-if (Test-Path $chocolateyScriptPath) {
-    Write-Host "Executing Chocolatey installation script..."
-    powershell -ExecutionPolicy Unrestricted -File $chocolateyScriptPath
-} else {
-    Write-Host "Chocolatey installation script not found at $chocolateyScriptPath"
-}
-
-# Check if the IIS script exists and execute it
-if (Test-Path $iisScriptPath) {
-    Write-Host "Executing IIS installation script..."
-    powershell -ExecutionPolicy Unrestricted -File $iisScriptPath
-} else {
-    Write-Host "IIS installation script not found at $iisScriptPath"
-}
-
-Write-Host "All scripts have been executed."
+#Install Chocolately packages in config file 
+choco install --yes packages.config --limitoutput --no-progress
